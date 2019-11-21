6e59cb480be6546dc1cb004dd5a6c48150ca0d0e
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-21 16:31:34 +0100

    Updates

---
+++ precache-manifest.b0e0bf30af29bdc154de9af32a998cde.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01722526681ab2e81399765c886225e1",
+    "url": "/charlesgourdin/homers-nervous-brickdown/index.html"
+  },
+  {
+    "revision": "e7808727bc8589b9d6c0",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/css/main.a7a0acf4.chunk.css"
+  },
+  {
+    "revision": "308eec141aa1cfce2512",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/js/2.401c5753.chunk.js"
+  },
+  {
+    "revision": "e7808727bc8589b9d6c0",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/js/main.982ed442.chunk.js"
+  },
+  {
+    "revision": "390adf3c0ae5f7b473ab",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/js/runtime-main.59c39360.js"
+  },
+  {
+    "revision": "700a23c4cd8535462d3751a56ba5acec",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/media/Donuts.700a23c4.png"
+  },
+  {
+    "revision": "9265bcc2ddc5609ed4a0ecb8b3d124d9",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/media/Duff.9265bcc2.png"
+  },
+  {
+    "revision": "514b4b363c997c4a75cdb5a6ae1c3975",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/media/Simpsonfont.514b4b36.ttf"
+  },
+  {
+    "revision": "a505266a355a699a7182ba4b3b42364d",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/media/bart_fall.a505266a.png"
+  },
+  {
+    "revision": "a81324354bac5f3cc4d36a1f7d4a9e42",
+    "url": "/charlesgourdin/homers-nervous-brickdown/static/media/toto.a8132435.mp3"
+  }
+]);
