e41ba4bdcd162cb6eea0b849581e00aec7df86d0
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-22 12:33:21 +0100

    Suppression de parameters (react modal non optimisé pour responsive)

--- src/components/Parameters.js
+++ src/components/Parameters.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Parameters.css';
 import ReactModal from 'react-modal';
-import { IoMdClose,
-         IoMdCog,
-         IoMdHelp,
-         IoIosVolumeHigh } from 'react-icons/io';
+import {
+  IoMdClose,
+  IoMdCog,
+  IoMdHelp,
+  IoIosVolumeHigh
+} from 'react-icons/io';
 import { TiArrowLeftThick, TiArrowRightThick } from "react-icons/ti";
 import totoUrl from '../musique/toto.mp3'
 import Duff from '../images/Duff.png'
@@ -15,7 +17,7 @@ import homer_tab_reverse from '../images/homer_tab_reverse.png'
 import heart from '../images/heart.png'
 
 class Parameters extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       showModal: false,
@@ -23,70 +25,90 @@ class Parameters extends React.Component {
       isPlaying: false,
     };
     this.toto = new Audio(totoUrl);
-    
+
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleOpenModal2 = this.handleOpenModal2.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.handleCloseModal2 = this.handleCloseModal2.bind(this);
   }
-  
-  handleOpenModal () {
+
+  handleOpenModal() {
     this.setState({ showModal: true });
   }
-  
-  handleOpenModal2 () {
+
+  handleOpenModal2() {
     this.setState({ showModal2: true });
   }
-  
-  handleCloseModal () {
+
+  handleCloseModal() {
     this.setState({ showModal: false });
   }
-  
-  handleCloseModal2 () {
+
+  handleCloseModal2() {
     this.setState({ showModal2: false });
   }
-  
+
   manageAudio = () => {
-    if (this.state.isPlaying){
+    if (this.state.isPlaying) {
       this.toto.pause();
-    }else {
+    } else {
       this.toto.play()
     }
     this.setState({ isPlaying: !this.state.isPlaying })
   }
-  
 
-  render () {
+
+  render() {
     return (
       <div className="block">
-        <button onClick={this.handleOpenModal}><IoMdHelp/></button>
-        <button onClick={this.handleOpenModal2}><IoMdCog/></button>
-        
-        <ReactModal 
-           style={{content: { background: "linear-gradient(180deg, rgba(3,141,247,1) 0%, rgba(120,194,251,1) 50%, rgba(251,251,251,1) 100%)", width:'70%'}}}
-           isOpen={this.state.showModal}
-           onRequestClose={this.handleCloseModal}>
-          <button onClick={this.handleCloseModal} style={{float:"right"}}><IoMdClose/></button>
-      
-            <h1 class="title">Tutorial</h1> 
-          <div style={{ margin: '30px'}}>
-            <p className="alignItem"><img src={homer_tab_clic1} alt='Donuts' className="homer_tab_clic1"/> = throw the ball</p>
-            <p className="alignItems"><img src={homer_donuts} alt='Donuts' className="homer_donuts"/>→<img src={homer_donuts_dooble} alt='Donuts' className="homer_donuts_dooble"/></p>
-            <p className="alignItems"><img src={Duff} alt='Duff' className="dufftutorial"/> = <img src={homer_tab_reverse} alt='Donuts' className="homer_tab_reverse"/></p>
-            <p className="alignItems"><TiArrowLeftThick className="iconsFlèche"/> = go to left</p>
-            <p className="alignItems"><TiArrowRightThick className="iconsFlèche"/> = go to right</p>
-            <p className="alignItems"><img src={heart} alt='heart' className="heartlife"/> = life</p>
-          </div>
-         </ReactModal>
+        <button onClick={this.handleOpenModal}><IoMdHelp /></button>
+        <button onClick={this.handleOpenModal2}><IoMdCog /></button>
 
-        <ReactModal 
-           style={{content: { background: "linear-gradient(180deg, rgba(3,141,247,1) 0%, rgba(120,194,251,1) 50%, rgba(251,251,251,1) 100%)", width:'70%'}}}
-           isOpen={this.state.showModal2}
-           onRequestClose={this.handleCloseModal2}
+        <ReactModal
+          style={{
+            content: {
+              background: "linear-gradient(180deg, rgba(3,141,247,1) 0%, rgba(120,194,251,1) 50%, rgba(251,251,251,1) 100%)",
+              width: '70%',
+              margin: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }
+          }}
+          isOpen={this.state.showModal}
+          onRequestClose={this.handleCloseModal}>
+
+
+          <h1 class="title">Tutorial</h1>
+            <div className="alignItem"><img src={homer_tab_clic1} alt='Donuts' className="homer_tab_clic1" /><p>= throw the ball</p></div>
+            <div className="alignItems"><img src={homer_donuts} alt='Donuts' className="homer_donuts" />→<img src={homer_donuts_dooble} alt='Donuts' className="homer_donuts_dooble" /></div>
+            <div className="alignItems"><img src={Duff} alt='Duff' className="dufftutorial" /> = <img src={homer_tab_reverse} alt='Donuts' className="homer_tab_reverse" /></div>
+            <div className="alignItems"><TiArrowLeftThick className="iconsFlèche" /><p> = go to left</p></div>
+            <div className="alignItems"><TiArrowRightThick className="iconsFlèche" /><p> = go to right</p></div>
+            <div className="alignItems"><img src={heart} alt='heart' className="heartlife" /><p> = life</p></div>
+            <button className = 'myButton' onClick={this.handleCloseModal} style={{}}><IoMdClose />Close</button>
+        </ReactModal>
+
+        <ReactModal
+          style={{
+            content: {
+              background: "linear-gradient(180deg, rgba(3,141,247,1) 0%, rgba(120,194,251,1) 50%, rgba(251,251,251,1) 100%)",
+              width: '70%',
+              margin: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }
+          }}
+          isOpen={this.state.showModal2}
+          onRequestClose={this.handleCloseModal2}
         >
-          <button onClick={this.handleCloseModal2} style={{ float: "right"}}><IoMdClose /></button>
+
           <h1 class="title">Parameters</h1>
-          <p class="x"><button onClick={this.manageAudio}><IoIosVolumeHigh className="iconSong"/></button>Music on/off</p>
+          <p class="x"><button onClick={this.manageAudio}><IoIosVolumeHigh className="iconSong" /></button>Music on/off</p>
+          <button className = 'myButton' onClick={this.handleCloseModal2} style={{}}><IoMdClose />Close</button>
         </ReactModal>
 
       </div>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './Homepage.css';
-import Parameters from '../components/Parameters';
 import { Link } from 'react-router-dom';
 
 class Homepage extends Component {
@@ -8,7 +7,6 @@ class Homepage extends Component {
     render() {
         return (
             <div className="homepage">
-                <Parameters />
                 <div className="buttons_homepage">
                     <button className="linktogame"> <Link to ="/Game">Play</Link></button>
                     <button className="linktogame" onClick={this.props.onClick}> <Link to ="/Game">Time Trial</Link></button>
@@ -21,4 +19,3 @@ class Homepage extends Component {
  
 
 export default Homepage;
-
