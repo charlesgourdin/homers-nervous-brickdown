da0a09cb108cefb22002ea2168d027b10a1a59e2
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-23 18:32:58 +0100

    Updates

---
+++ precache-manifest.bdfba812f03c17b69f9247296b021872.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfd42bb7cf3890011953d0c89b22e33",
+    "url": "/homers-nervous-brickdown/index.html"
+  },
+  {
+    "revision": "ca8b8d9e1c2fb9c37f6f",
+    "url": "/homers-nervous-brickdown/static/css/main.1d36b61c.chunk.css"
+  },
+  {
+    "revision": "e80b12b9fb7a40f896ca",
+    "url": "/homers-nervous-brickdown/static/js/2.844155bc.chunk.js"
+  },
+  {
+    "revision": "ca8b8d9e1c2fb9c37f6f",
+    "url": "/homers-nervous-brickdown/static/js/main.5a5cdf20.chunk.js"
+  },
+  {
+    "revision": "b4d367bf9cadb46efce1",
+    "url": "/homers-nervous-brickdown/static/js/runtime-main.9af48499.js"
+  },
+  {
+    "revision": "700a23c4cd8535462d3751a56ba5acec",
+    "url": "/homers-nervous-brickdown/static/media/Donuts.700a23c4.png"
+  },
+  {
+    "revision": "9265bcc2ddc5609ed4a0ecb8b3d124d9",
+    "url": "/homers-nervous-brickdown/static/media/Duff.9265bcc2.png"
+  },
+  {
+    "revision": "514b4b363c997c4a75cdb5a6ae1c3975",
+    "url": "/homers-nervous-brickdown/static/media/Simpsonfont.514b4b36.ttf"
+  },
+  {
+    "revision": "37d1d68b77c6e5439717a27315958c7a",
+    "url": "/homers-nervous-brickdown/static/media/background-home.37d1d68b.jpg"
+  },
+  {
+    "revision": "33759bdc7f5b2891eccdd8f73644ab34",
+    "url": "/homers-nervous-brickdown/static/media/background.33759bdc.jpg"
+  },
+  {
+    "revision": "b8f9462adbb595d41185b9d9612c6ac8",
+    "url": "/homers-nervous-brickdown/static/media/bart_fall.b8f9462a.png"
+  },
+  {
+    "revision": "873cfaedef531815d45f5e720aacbe8b",
+    "url": "/homers-nervous-brickdown/static/media/bart_skate1.873cfaed.jpg"
+  },
+  {
+    "revision": "5443d3e992762bbf3f2f1c947a780a34",
+    "url": "/homers-nervous-brickdown/static/media/brick.5443d3e9.mp3"
+  },
+  {
+    "revision": "3bfc4cb264a9b8a89b9005bcd13d4e41",
+    "url": "/homers-nervous-brickdown/static/media/doh.3bfc4cb2.mp3"
+  },
+  {
+    "revision": "5e6920b7d8e77a337240a3488eaff7f9",
+    "url": "/homers-nervous-brickdown/static/media/donuts.5e6920b7.mp3"
+  },
+  {
+    "revision": "f8f0ce0a30dc3a555e820143d8c7a471",
+    "url": "/homers-nervous-brickdown/static/media/homer_donuts.f8f0ce0a.png"
+  },
+  {
+    "revision": "3c6df242a0af7eaed3e8f94d16871102",
+    "url": "/homers-nervous-brickdown/static/media/homer_donuts_dooble.3c6df242.png"
+  },
+  {
+    "revision": "c6fa67ab574bf15d2e58403626109033",
+    "url": "/homers-nervous-brickdown/static/media/homer_tab.c6fa67ab.png"
+  },
+  {
+    "revision": "ef4abf9a542783cd0f163e8a5879510b",
+    "url": "/homers-nervous-brickdown/static/media/homer_tab_clic1.ef4abf9a.png"
+  },
+  {
+    "revision": "56bf5753323385f33950f850f5ef185f",
+    "url": "/homers-nervous-brickdown/static/media/homer_tab_reverse.56bf5753.png"
+  },
+  {
+    "revision": "4e5f83fcd8cec03eb75cbeae5b7e2413",
+    "url": "/homers-nervous-brickdown/static/media/lostgame.4e5f83fc.jpg"
+  }
+]);
