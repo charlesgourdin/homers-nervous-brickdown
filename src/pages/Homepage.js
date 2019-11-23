4a40c2983903e2b6ea354c0c9c788ade4b3c1d7d
Charles Gourdin <gourdin.charles@gmail.com>
2019-11-23 17:29:44 +0100

    commit avant suppression de react modal pour opoup fin de partie

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -12,7 +12,7 @@ class Homepage extends Component {
     constructor(props){
         super(props)
         this.state={
-            displayMenu: true
+            displayMenu: false
         }
     }
 
